b1bcc623430f5dc0a8415ce66683198567c6c168
Sasa <sasa.popravak@gmail.com>
2021-02-22 18:43:17 +0100

    b1

---
+++ src/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "../src/print_function.h"
+
+int main (void) {
+
+   print_from_function ();
+
+   return 0;
+}
+
---
+++ src/print_function.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int print_from_function () {
+
+    printf ("\nprogram2: b1.\n");
+
+    return 0;
+}
